75dcc2739b828942ef5809124a287967bf75a142
Andrey <admin@twister-vl.ru>
2018-02-27 15:22:22 +0300

    Add darwin unsigned build

--- build.sh
+++ build.sh
@@ -19,6 +19,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     electron-packager . BitcoenWallet --platform=linux --icon=logo.ico --out=build --overwrite
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     electron-packager . BitcoenWallet --platform=darwin --icon=logo.ico --out=build --overwrite --version=0.35.6 --app-bundle-id="com.bitcoen.bitcoenwallet" --app-version="1.0.0" --build-version="1.0.000" --osx-sign
+    electron-packager . BitcoenWalletUnsigned --platform=darwin --icon=logo.ico --out=build --overwrite
 elif [[ "$OSTYPE" == "msys" ]]; then
      electron-packager . BitcoenWallet --platform=win32 --icon=logo.ico --out=build --overwrite
 else
@@ -46,6 +47,17 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     cd ../installers/
     rm -R BitcoenWallet-darwin-x64.dmg
     appdmg dmg.json BitcoenWallet-darwin-x64.dmg
+    
+    cd ..
+    cp -R core BitcoenWalletUnsigned-darwin-x64/BitcoenWalletUnsigned.app/Contents/Resources/app/
+    cp -f ../buildBinary/node_darwin BitcoenWalletUnsigned-darwin-x64/BitcoenWalletUnsigned.app/Contents/Resources/app/core/node
+    chmod 777 BitcoenWalletUnsigned-darwin-x64/BitcoenWalletUnsigned.app/Contents/Resources/app/core/node
+
+    rm -R ../installers/BitcoenWalletUnsigned.app
+    cp -R BitcoenWalletUnsigned-darwin-x64/BitcoenWalletUnsigned.app ../installers/
+    cd ../installers/
+    rm -R BitcoenWalletUnsigned-darwin-x64.dmg
+    appdmg dmg.json BitcoenWalletUnsigned-darwin-x64.dmg
 fi
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
