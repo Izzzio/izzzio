451fadc1f52da04613edc07f5d8105f9a9751280
Kirill <revenkov-k@mail.ru>
2018-08-09 10:47:13 +0300

    remake validators

--- modules/validators.js
+++ modules/validators.js
@@ -10,19 +10,24 @@
 
 /*2. Добавить для объекта Blockchain объект, содержащий в себе массив консенсус (из конфига), и массив модулей и метод, который позволяет добавлять в этот массив модуль в формате название - версия, messageBusAddress*/
 'use strict';
-const validatorsPath = './validators/';
 const fs = require('fs');
+const path = require('path');
+const validatorsDir = 'validators';
+const validatorsPath = path.join(__dirname, validatorsDir);
+
 
 class Validators {
     constructor (config){
         this.validators = [];
         this.modules = [];
+        this.versions = [];
         for (let v of config.validators){
             this.addModule(v);
         }
+        this.messageBusAddress = config.recieverAddress;
     };
     
-    addModule(moduleName = '') {
+   /* addModule(moduleName = '') {
         try {
             this.validators.push(moduleName);
             this.modules[moduleName] = require(validatorsPath + moduleName);
@@ -31,7 +36,26 @@ class Validators {
             console.log(e);
             console.log('Info: Adding canceled becauseof error');
         }
-    }; 
+    }; */
+    
+    /**
+     * add new module description
+     * @param {object} moduleName      
+     * @param {string} version = '0.0' 
+     */
+    addModule(moduleName, version = '0.0'){
+        let modulePath = path.join(validatorsPath, moduleName + '.js');
+        //check is there such file in module directory. if not then we don't add this module
+        try {
+            fs.accessSync(modulePath, fs.constants.R_OK);
+            this.validators.push(moduleName);       //array of validator's names
+            this.modules[moduleName] = path.join(validatorsPath, moduleName); //array of modules
+            this.versions[moduleName] = version;
+        } catch (err) {
+            console.log(err);
+            console.log('Info: Adding canceled because of error');
+        }
+    };
 };
     
 module.exports = Validators;
