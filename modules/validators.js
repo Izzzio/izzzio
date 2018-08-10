f21a4c3fcfccf972028e17fea2d89612d077922a
Kirill <revenkov-k@mail.ru>
2018-08-10 08:15:24 +0300

    corrected validators class

--- modules/validators.js
+++ modules/validators.js
@@ -8,7 +8,6 @@
  */
 
 
-/*2. Добавить для объекта Blockchain объект, содержащий в себе массив консенсус (из конфига), и массив модулей и метод, который позволяет добавлять в этот массив модуль в формате название - версия, messageBusAddress*/
 'use strict';
 const fs = require('fs');
 const path = require('path');
@@ -27,34 +26,14 @@ class Validators {
         this.messageBusAddress = config.recieverAddress;
     };
     
-   /* addModule(moduleName = '') {
-        try {
-            this.validators.push(moduleName);
-            this.modules[moduleName] = require(validatorsPath + moduleName);
-            console.log('Info: ' + moduleName + ' was added to validators array');
-        } catch (e) {
-            console.log(e);
-            console.log('Info: Adding canceled becauseof error');
-        }
-    }; */
-    
     /**
      * add new module description
      * @param {object} moduleName      
      * @param {string} version = '0.0' 
      */
     addModule(moduleName, version = '0.0'){
-        let modulePath = path.join(validatorsPath, moduleName + '.js');
-        //check is there such file in module directory. if not then we don't add this module
-        try {
-            fs.accessSync(modulePath, fs.constants.R_OK);
-            this.validators.push(moduleName);       //array of validator's names
-            this.modules[moduleName] = path.join(validatorsPath, moduleName); //array of modules
-            this.versions[moduleName] = version;
-        } catch (err) {
-            console.log(err);
-            console.log('Info: Adding canceled because of error');
-        }
+        this.validators.push(moduleName);       
+        this.versions[moduleName] = version;
     };
     
     /**
