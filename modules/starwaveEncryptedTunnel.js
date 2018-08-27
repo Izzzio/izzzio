b83d8dff2593d498253ccc472fba013f55b12f72
Kirill <revenkov-k@mail.ru>
2018-08-27 11:46:29 +0300

    First steps

---
+++ modules/starwaveEncryptedTunnel.js
@@ -0,0 +1,69 @@
+/**
+ * Encription protocol for starwave
+ *
+ *
+ *
+ **/
+'use strict';
+
+const crypto = require("crypto");
+
+class StarwaveEncryptedTunnel {
+    /*constructor(bits = 2048){
+        //diffiehellman object
+        this.keyObject = crypto.createDiffieHellman(bits);
+        this.public = this.generateKeys();
+    };*/
+
+    constructor(curve = 'secp521r1'){
+        // edcaobject
+        this.keyObject = crypto.createECDH(curve);
+        this.public = this.generateKeys();
+    };
+
+    generateKeys (){
+        let publicKey = this.keyObject.generateKeys('hex');
+        return publicKey;
+    }
+
+    createSecret(externalPublic){
+        let secret = this.keyObject.computeSecret(externalPublic, 'hex', 'hex');
+        return secret;
+    };
+
+    cipherData(data, secret, algorithm = 'aes256'){
+        const cipher = crypto.createCipher(algorithm, secret);
+        let encrypted = cipher.update(data,'utf8', 'hex');
+        encrypted += cipher.final('hex');
+        return encrypted;
+    }
+
+    decipherData(encryptedData, secret, algorithm = 'aes256'){
+        const decipher = crypto.createDecipher(algorithm, secret);
+        let data = decipher.update(encryptedData,'hex', 'utf8');
+        data += decipher.final('utf8');
+        return data;
+    }
+
+}
+
+console.log('start');
+let st1 = new StarwaveEncryptedTunnel();
+console.log(st1);
+console.log(st1.public.toString('hex'));
+console.log('');
+let st2 = new StarwaveEncryptedTunnel();
+console.log(st2);
+console.log('');
+
+let s1 = st1.createSecret(st2.public);
+let s2 = st2.createSecret(st1.public);
+
+console.log('secrets');
+console.log(s1);
+console.log(s2);
+
+
+
+/*console.log('secret keys');
+let sk1 = st.createSecret(k2.public)*/
