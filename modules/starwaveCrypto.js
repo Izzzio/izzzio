ea19d26080e35255a10c7f18f4542a08cd19273f
Kirill <revenkov-k@mail.ru>
2018-08-28 14:02:32 +0300

    add comments and JSON parse/stringify data before (de)ciphering

--- modules/starwaveCrypto.js
+++ modules/starwaveCrypto.js
@@ -24,16 +24,36 @@ class StarwaveCrypto {
         this.starwave = starwave;
     };
 
+    /**
+     * generate keys object for Diffie-Hellman
+     * @returns {*}
+     */
     generateKeys (){
         let publicKey = this.keyObject.generateKeys('hex');
         return publicKey;
     }
 
+    /**
+     * create secret key based on external public key
+     * @param externalPublic
+     * @returns {*}
+     */
     createSecret(externalPublic){
-        let secret = this.keyObject.computeSecret(externalPublic, 'hex', 'hex');
+        let secret;
+        try {
+            secret = this.keyObject.computeSecret(externalPublic, 'hex', 'hex');
+        } catch (err) {
+            console.log('Can not create secret key ' + err); //if externalPublic is wrong
+        }
         return secret;
     };
 
+    /**
+     * Encrypts data
+     * @param data
+     * @param secret
+     * @param algorithm
+     */
     cipherData(data, secret, algorithm = 'aes256'){
         const cipher = crypto.createCipher(algorithm, secret);
         let encrypted = cipher.update(data,'utf8', 'hex');
@@ -41,6 +61,13 @@ class StarwaveCrypto {
         return encrypted;
     }
 
+    /**
+     * Decripts data
+     * @param encryptedData
+     * @param secret
+     * @param algorithm
+     * @returns {*}
+     */
     decipherData(encryptedData, secret, algorithm = 'aes256'){
         const decipher = crypto.createDecipher(algorithm, secret);
         let data = decipher.update(encryptedData,'hex', 'utf8');
@@ -48,6 +75,11 @@ class StarwaveCrypto {
         return data;
     }
 
+    /**
+     * decrypts data field in message
+     * @param message
+     * @returns {*}
+     */
     decipherMessage(message){
         let decryptedData;
 
@@ -56,41 +88,65 @@ class StarwaveCrypto {
             return decryptedData = message.data;
         }
 
-        //if we have secret key associated with this socket than we have th e tunnel
+        //if we have secret key associated with this socket than we have the tunnel
         if (this.starwave.blockchain.secretKeys[message.sender]){
             decryptedData = this.decipherData(message.data, this.starwave.blockchain.secretKeys[message.sender]);
+            //try to parse json
+            try{
+                decryptedData = JSON.parse(decryptedData)
+            } catch (e) {
+                console.log("An error occurred while parsing decrypted text: " + e);
+            }
             message.data = decryptedData;
             delete message.encrypted;
         }
         return decryptedData;
     }
 
+    /**
+     * encrypts data field in message
+     * @param message
+     * @returns {*}
+     */
     cipherMessage(message){
         let cryptedData;
         //should be assotiated secret key and check that message has not been already encrypted
         if (this.starwave.blockchain.secretKeys[message.reciver] && !message.encrypted){
-            cryptedData = this.cipherData(message.data, this.starwave.blockchain.secretKeys[message.reciver]);
+            cryptedData = this.cipherData(JSON.stringify(message.data), this.starwave.blockchain.secretKeys[message.reciver]);
             message.encrypted = true;
             message.data = cryptedData;
         }
         return cryptedData;
     }
 
+    /**
+     * processes incoming message
+     * @param message
+     * @returns {*}
+     */
     handleIncomingMessage(message){
         //watch if the message has Public key field then the message is only for sending the key
         if (message.publicKey) {
+            let sc = this.createSecret(message.publicKey); //exclude situation with exception on wrong public
             //if we don't have secret key then we save sender publickey and create secret and send our public to make connection
-            if (!this.starwave.blockchain.secretKeys[message.sender]){
+            if (!this.starwave.blockchain.secretKeys[message.sender] && sc){
                 this.makeConnection(message.sender);
             }
-            this.starwave.blockchain.secretKeys[message.sender] = this.createSecret(message.publicKey);
+            if (sc){
+                this.starwave.blockchain.secretKeys[message.sender] = sc;
+            }
             delete message.publicKey;
             return 0;
         }
         //try to decipher message if it possible
-        return this.decipherMessage(message, socket); //undefined if we have errors
+        return this.decipherMessage(message); //undefined if we have errors
     }
 
+    /**
+     * send public key to messagebus address
+     * @param messageBus
+     * @returns {{data: *, reciver: *, sender: *, id: *, timestamp: number, TTL: number, index: *, mutex: string, relevancyTime: Array, route: Array, type: number, timestampOfStart: number}|any|{type: number, data: *, reciver: *, recepient: *, id: *, timestamp: number, TTL: number, index: number}}
+     */
     makeConnection(messageBus){
         let message = this.starwave.createMessage('',messageBus,this.starwave.config.recieverAddress,'DH-CONNECTION');
         message.publicKey = this.public;
@@ -98,13 +154,18 @@ class StarwaveCrypto {
         return message;
     }
 
+    /**
+     * send message using encryption protocol
+     * @param message
+     * @returns {number}
+     */
     sendMessage(message){
         //check if we have secret key for reciver
         let sk = this.starwave.blockchain.secretKeys[message.reciver]; //secret key
         if (sk){
             if (this.cipherMessage(message)){
                 this.starwave.sendMessage(message);
-                return 0;
+                return message;
             }else{
                 console.log("Error: Can't cipher message");
                 return 2;
@@ -112,7 +173,7 @@ class StarwaveCrypto {
         }
         else
         {
-            console.log(`Error: there is no secret key for ${message.reciver}`)
+            console.log(`Error: there is no secret key for address: ${message.reciver}`)
             return 1;
         }
     }
