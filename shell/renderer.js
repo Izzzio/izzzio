3b9c31dc3b6e7efb3803d04d236afc9c5ce4a4de
Andrey <admin@twister-vl.ru>
2018-09-14 01:09:23 +0300

    Cleanup

--- shell/renderer.js
+++
@@ -1,8 +0,0 @@
-function log(logStr) {
-    logStr = String(logStr).split("\n").reverse().join("<br>");//.replace(/\n/g, '<br>');
-    document.getElementById("log").innerHTML = logStr + document.getElementById("log").innerHTML;
-}
-
-require('electron').ipcRenderer.on('log', function (event, message) {
-    log(message);
-});
