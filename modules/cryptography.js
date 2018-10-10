701a262407ef36457a60faf8b1da82118be71454
Kirill <revenkov-k@mail.ru>
2018-10-10 10:15:09 +0300

    add key generation and sign

--- modules/cryptography.js
+++ modules/cryptography.js
@@ -5,13 +5,32 @@
 'use strict';
 const CryptoJS = require('crypto-js');
 const GOST = new (require('./gost'))();
+const GostSign = require('./gostSign');
+const inputOutputFormat = 'hex';
+const SIGN_TYPE = 'sha256';
+const crypto = require('crypto');
+
+
+/**
+ * Repair bad generated key
+ * @param key
+ * @return {*}
+ */
+function repairKey(key) {
+    if(key[key.length - 1] !== "\n") {
+        key += "\n";
+    }
+    return key.replace(new RegExp("\n\n", 'g'),"\n");
+}
+
+
 
 class Cryptography{
     constructor(config){
-        //в любой непонятной ситуации используем SHA256
         if (!config){
-            this.hashFunction = CryptoJS.SHA256 ;
+            this.hashFunction = CryptoJS.SHA256 ; // используем SHA256 как хэш по умолчанию
         } else {
+            //настраиваем хэш
             switch (config.hashFunction) {
                 case 'SHA256':
                     this.hashFunction = CryptoJS.SHA256;
@@ -26,9 +45,84 @@ class Cryptography{
                     this.hashFunction = CryptoJS.SHA256;
                     break;
             }
+            let a = {};
+           /* //настраиваем генерацию ключей
+
+            switch (config.keyGenerationFunction){
+                case 'GOST':
+                    a = {hash: "GOST R 34.11", length: 256};
+                    break;
+                case 'GOST512':
+                    a = {hash: "GOST R 34.11", length: 512, namedCurve: "T-512-A"};
+                    break;
+            }*/
+            //настраиваем подпись
+            switch (config.signFunction) {
+                case 'GOST':
+                    a = {hash: "GOST R 34.11", length: 256};
+                    break;
+                case 'GOST512':
+                    a = {hash: "GOST R 34.11", length: 512, namedCurve: "T-512-A"};
+                    break;
+            }
+            if (a !== {}){
+                this.gostSign = new GostSign(a);
+            }
         }
     }
 
+    /**
+     * generates pair of keys
+     * @returns {{private: *, public: *}}
+     */
+    generateKeyPair(){
+        let keyPair;
+        if (this.gostSign) {
+            keyPair = this.gostSign.generateKey();
+            //конвертируем в формат
+            keyPair.public = Buffer.from(keypair.publiKey).toString(inputOutputFormat);
+            keyPair.private = Buffer.from(keypair.privateKey).toString(inputOutputFormat);
+        } else {
+            keyPair = keypair({bits: 2048});
+            keyPair.private = repairKey(keyPair.private);
+            keyPair.public = repairKey(keyPair.public);
+        }
+        return {private: keyPair.private, public: keyPair.public};
+    }
+
+    /**
+     * signs data
+     * @param data
+     * @param key
+     * @returns {{data: *, sign: *}}
+     */
+    sign(data, key){
+        let signedData;
+        if (this.gostSign) {
+            let bData, bKey;
+            try{
+                bData = Buffer.from(data);
+            } catch (e) {
+                bData = Buffer.from(JSON.stringify(data));
+            }
+            bKey = Buffer.from(key, inputOutputFormat);
+            signedData = this.gostSign.sign(bKey, bData);
+            signedData = Buffer.from(signedData).toString(inputOutputFormat);
+        } else {
+            key = repairKey(key);
+            const _sign = crypto.createSign(SIGN_TYPE);
+            _sign.update(data);
+            let signedData = _sign.sign(key).toString(inputOutputFormat);
+        }
+        return {data: data, sign: signedData};
+    }
+
+    /**
+     * creates hash of the data
+     * @param data
+     * @param hashFunction
+     * @returns {*}
+     */
     hash(data, hashFunction){
         if (!data){
             return false;
