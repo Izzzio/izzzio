78e77ad24240040646dfe00ffedd4591b81c241c
Kirill <Revenkov-k@mail.ru>
2018-10-19 16:54:37 +0300

    add utils

--- modules/utils.js
+++ modules/utils.js
@@ -27,5 +27,38 @@ module.exports = {
         setTimeout(function () {
             waitForSync();
         }, 10);
+    },
+    /**
+     * Преобразует 16 ричное число с длиной кратной 4 в строку символов UTF-8
+     * @param str
+     * @return {*}
+     */
+    hexString2Unicode: function (str) {
+        if(str.length % 4 !== 0){
+            return false;
+        }
+        str = str.toLowerCase();
+        let code = '';
+        str = str.match(/.{1,4}/g);
+        for(let s of str){
+            if(s.length === 4) {
+                code += String.fromCharCode(parseInt(s, 16));
+            }else{
+                code += s;
+            }
+        }
+        return code;
+    },
+    /**
+     * Преобразует строку UTF-8 в строку с 16 ричным числом
+     * @param uniStr
+     * @return {string}
+     */
+    unicode2HexString: function (uniStr) {
+        let str = '';
+        for(let i = 0; i<uniStr.length; i++){
+            str += uniStr.charCodeAt(i).toString(16);
+        }
+        return str;
     }
 };
