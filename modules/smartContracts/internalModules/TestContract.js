7b6ae923660751028b8ed866e165e10262487121
Kirill <Revenkov-k@mail.ru>
2018-12-05 15:42:00 +0300

    Create TestContract.js

---
+++ modules/smartContracts/internalModules/TestContract.js
@@ -0,0 +1,19 @@
+const OWNER = false;
+const EMISSION = 100;
+const TOKEN_NAME = 'TESTTOKEN';
+const TICKER = 'TESTICKER';
+
+
+/**
+ * Test token contract
+ */
+class TestContract extends TokenContract {
+    init (totalEmission = EMISSION) {
+        this.contract = {
+            name: TOKEN_NAME,
+            ticker: TICKER,
+            owner: OWNER
+        };
+        super.init(totalEmission);
+    };
+}
