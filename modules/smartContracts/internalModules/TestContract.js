24ece646a984d8c2d83637d5e53fd08ff0147b87
Kirill Revenkov <revenkov-k@mail.ru>
2018-12-06 23:04:20 +0300

    Update TestContract.js

--- modules/smartContracts/internalModules/TestContract.js
+++ modules/smartContracts/internalModules/TestContract.js
@@ -3,11 +3,11 @@ const EMISSION = 100;
 const TOKEN_NAME = 'TESTTOKEN';
 const TICKER = 'TESTICKER';
 
-
 /**
  * Test token contract
  */
 class TestContract extends TokenContract {
+
     init (totalEmission = EMISSION) {
         super.init(totalEmission);
     };
@@ -19,6 +19,26 @@ class TestContract extends TokenContract {
             owner: OWNER
         };
     }
+
+    transfer(to, amount, from = state.from) {
+        this.wallets.transfer(from, to, amount);
+        this.TransferEvent.emit(from, to, new BigNumber(amount));
+    }
+
+    /**
+     * Make transfer to external contract
+     * @param address
+     * @param amount
+     * @param method
+     * @param args
+     */
+    makeTransferToExternal(address, amount, method, args) {
+        this.transfer(address, amount);
+        let connector = new TokenContractConnector(address);
+        connector.registerDeployMethod(method, method);
+        return connector[method](args);
+    }
+
 }
 
 global.registerContract(TestContract);
