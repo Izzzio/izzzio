10d20ba84c09e6a7843b22456ea89ffe1c639fe1
Andrey <admin@twister-vl.ru>
2018-12-25 21:11:09 +0300

    DLCPoA Sigma

--- modules/validators/dlcpoa.js
+++ modules/validators/dlcpoa.js
@@ -11,12 +11,14 @@
 
 
 const TARGET_NETWORK_SPEED = 100;                       // blocks per second
-const MINIMAL_COMPLEXITY = 0.1;                         //minimal network complexity
+const MINIMAL_COMPLEXITY = 0.0;                         //minimal network complexity
 const MAX_HASH_SIGMA = 'ffffff';                        //Maximum hash sigma
+const MAX_VARIANTS_THRESHOLD = 3000;                     //Maximum valid hash variants
 
 //******************************* Auto calculated ****************************************
-const MAX_HASH_SIGMA_INT = Number.parseInt(MAX_HASH_SIGMA, 16);//Minimal complexity sigma
-const HASH_SIGMA_LENGTH = MAX_HASH_CHECK.length;                    //How many symbols form hash use for validation
+//const MAX_HASH_SIGMA_INT = Number.parseInt(MAX_HASH_SIGMA, 16);//Minimal complexity sigma
+const HASH_SIGMA_LENGTH = MAX_HASH_SIGMA.length;                    //How many symbols form hash use for validation
+
 
 /**
  * Blockchain object
@@ -28,6 +30,10 @@ const Block = require('../block');
 const Signable = require('../blocks/signable');
 
 const moment = require('moment');
+const logger = require('../logger')('DLCPoA');
+
+
+let genesisHash = false;
 
 /**
  * Проверка корректности блока
@@ -44,27 +50,28 @@ function isValidNewBlock(newBlock, previousBlock) {
     complexity = complexity < MINIMAL_COMPLEXITY ? MINIMAL_COMPLEXITY : complexity;
     complexity = Math.round(complexity * 10000) / 10000;
 
+    let newHashValid = isValidHash(newBlock.hash, complexity);
 
     if(previousBlock.index + 1 !== newBlock.index) {
-        console.log('Error: DLCPoA: Invalid block index ' + newBlock.index);
+        logger.error('DLCPoA: Invalid block index ' + newBlock.index);
         return false;
-    } else if((previousBlock.hash !== newBlock.previousHash) || !isValidHash(newBlock.hash)) {
-        console.log('Error: DLCPoA: Invalid block previous hash or new hash in ' + newBlock.index);
+    } else if((previousBlock.hash !== newBlock.previousHash) || !newHashValid) {
+        logger.error('DLCPoA: Invalid block previous hash or new hash in ' + newBlock.index);
         return false;
-    } else if(!isValidHash(previousBlock.hash) && previousBlock.sign.length === 0) {
-        console.log('Error: DLCPoA: Invalid previous block hash');
+    } else if(!isValidHash(previousBlock.hash, complexity) && previousBlock.sign.length === 0) {
+        logger.error('DLCPoA: Invalid previous block hash');
         return false;
-    } else if((blockchain.calculateHashForBlock(newBlock) !== newBlock.hash) || !isValidHash(newBlock.hash, complexity)) {
-        console.log('Error: DLCPoA: Invalid hash for block: ' + blockchain.calculateHashForBlock(newBlock) + ' ' + newBlock.hash);
+    } else if((blockchain.calculateHashForBlock(newBlock) !== newBlock.hash) || !newHashValid) {
+        logger.error('DLCPoA: Invalid hash for block: ' + blockchain.calculateHashForBlock(newBlock) + ' ' + newBlock.hash);
         return false;
     } else if(newBlock.startTimestamp > newBlock.timestamp || previousBlock.timestamp > newBlock.timestamp) { //LCPoA time checking
-        console.log('Error: DLCPoA: Invalid start or block timestamp');
+        logger.error('DLCPoA: Invalid start or block timestamp');
         return false;
     } else if(newBlock.timestamp > (moment().utc().valueOf() + 1000)) {
-        console.log('Error: DLCPoA: Invalid local time or block creator time');
+        logger.error('DLCPoA: Invalid local time or block creator time');
         return false;
     } else if(String(newBlock.timestamp).length !== 13 || String(newBlock.startTimestamp).length !== 13) {
-        console.log('Error: DLCPoA: Invalid timestamp number');
+        logger.error('DLCPoA: Invalid timestamp number');
         return false;
     }
 
@@ -101,10 +108,17 @@ function getComplexity(currentBlockTime, previousBlockTime) {
  * @returns {boolean}
  */
 function isValidHash(hash, complexity) {
+    if(hash === genesisHash) {
+        return true;
+    }
     let hashSigma = hash.slice(-HASH_SIGMA_LENGTH);
     hashSigma = Number.parseInt(hashSigma, 16);
 
-    return hashSigma <= Math.round(MAX_HASH_SIGMA_INT * (1 - complexity));
+    // return hashSigma <= Math.round(MAX_HASH_SIGMA_INT * (1 - complexity));
+
+    //console.log('Hash sigma', hashSigma, Math.round(MAX_VARIANTS_THRESHOLD - (MAX_VARIANTS_THRESHOLD * complexity)));
+
+    return hashSigma <= Math.round(MAX_VARIANTS_THRESHOLD - (MAX_VARIANTS_THRESHOLD * complexity));
 }
 
 
@@ -128,7 +142,7 @@ function generateNextBlock(blockData, cb, cancelCondition) {
 
     let nextHash = '';
     let nextTimestamp;
-    console.log('Mining: Mining block...');
+    logger.info('Mining: Mining block...');
     blockchain.miningNow++;
     blockchain.setMiningForce(blockchain.miningNow, blockchain.miningForce);
     let mineCounter = 0;
@@ -136,6 +150,7 @@ function generateNextBlock(blockData, cb, cancelCondition) {
     let lastTime = new Date().getTime() / 1000;
     let minerNo = blockchain.miningNow;
     let startTimestamp = moment().utc().valueOf();
+
 
     function tryMine() {
         if(typeof cancelCondition !== 'undefined') {
@@ -150,9 +165,16 @@ function generateNextBlock(blockData, cb, cancelCondition) {
                 //В этом случае скорее всего сеть занята синхронизацией, и надо перенести майнинг на попозже
                 setTimeout(tryMine, 5000);
             }
+
+
             const nextIndex = previousBlock.index + 1;
             nextTimestamp = moment().utc().valueOf();
             nextHash = blockchain.calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData, startTimestamp, '');
+
+            let complexity = getComplexity(nextTimestamp, previousBlock.timestamp);
+            complexity = complexity < MINIMAL_COMPLEXITY ? MINIMAL_COMPLEXITY : complexity;
+            complexity = Math.round(complexity * 10000) / 10000;
+
             if(nextHash !== lastHash) {
                 lastHash = nextHash;
                 mineCounter++;
@@ -160,12 +182,12 @@ function generateNextBlock(blockData, cb, cancelCondition) {
             if((nextTimestamp / 1000) % 1 === 0) {
                 blockchain.miningForce = (mineCounter / 1);
                 blockchain.setMiningForce(blockchain.miningNow, blockchain.miningForce);
-                console.log('Miner #' + minerNo + ': Speed ' + (mineCounter / 1) + ' H/s');
+                logger.info('Miner #' + minerNo + ': Speed ' + (mineCounter / 1) + ' H/s | Complexity ' + Math.round(complexity * 100) + '%');
                 lastTime = nextTimestamp / 1000;
                 mineCounter = 0;
             }
-            if(isValidHash(nextHash)) {
-                console.log('Mining: New block found ' + nextHash);
+            if(isValidHash(nextHash, complexity)) {
+                logger.info('Mining: New block found ' + nextHash + ' in '+((nextTimestamp - startTimestamp)/1000)+' seconds | Complexity ' + Math.round(complexity * 100) + '%');
                 blockchain.miningNow--;
                 blockchain.setMiningForce(blockchain.miningNow, blockchain.miningForce);
                 cb(new Block(nextIndex, previousBlock.hash, nextTimestamp, blockData, nextHash, startTimestamp, ''));
@@ -211,7 +233,8 @@ function setGenerateEmptyBlocks(generate) {
 
 module.exports = function (blockchainVar) {
     blockchain = blockchainVar;
-    console.log('Info: DLCPoA validator loaded');
+    genesisHash = blockchain.getGenesisBlock().hash;
+    logger.info('DLCPoA validator loaded');
     return {
         consensusName: 'DLCPoA',
         isValidNewBlock: isValidNewBlock,
