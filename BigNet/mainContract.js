10f0caaceb2981f52d8c4a10924c351682e43a0b
Andrey <admin@twister-vl.ru>
2019-02-12 01:17:21 +0300

    Contract address back

--- BigNet/mainContract.js
+++ BigNet/mainContract.js
@@ -32,7 +32,13 @@ const TICKER = 'iZ3';
  * Address of main contract owner
  * @type {string}
  */
-const CONTRACT_OWNER = 'izM1Tr1nhKaeDMqUaZjHqaWzjZmCndnUhML';
+const CONTRACT_OWNER = '-----BEGIN RSA PUBLIC KEY-----\n' +
+    'MIIBCgKCAQEApSJ2Lm6h26vHgiqB4VcyOZE+meRB6Jaow6Z+6cBn43fvcM57l8O2DfFTgo9R\n' +
+    '4AUavuFJU8bekhcCWYC53RErumjHBrWVviGDOxRALfev8fOU6V+hm9E7FGiW5RXMew5729lt\n' +
+    'rOxsyrayUtBLsd6BAEO5n/AtAI08Et403X/UX/7N/9zKu+F2E/fi1VlvJS07TtgPoRuT9vx6\n' +
+    'ol7B0OcqGU0lIe84TasfX4pN9RIZe3+O8idBTg9aHbtuD2qSSQ9x1jpcD4wOtb+FhgBJ3dOI\n' +
+    'eIC3eapWvK4HFtAtX1uIyod3LruSVssrNtLEqWOgD5MwOlv1KWAR0ZDZ3cYNL8Of8QIDAQAB\n' +
+    '-----END RSA PUBLIC KEY-----\n';
 
 /**
  * Minimal zero-cost resources
